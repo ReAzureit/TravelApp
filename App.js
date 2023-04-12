92adab85564d6968b27b2fd4d0649fb732f3db28
ReAzureit <ramdanirafi412@gmail.com>
2023-04-12 08:10:28 +0700

    feat(navigation): add navigation setup

--- App.js
+++ App.js
@@ -1,8 +1,17 @@
 import React from "react";
+import "react-native-gesture-handler";
 import Home from "./src/screens/Home";
-
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+const Stack = createStackNavigator();
 const App = () => {
-	return <Home />;
+	return (
+		<NavigationContainer>
+			<Stack.Navigator screenOptions={{ headerShown: false }}>
+				<Stack.Screen name="Home" component={Home} />
+			</Stack.Navigator>
+		</NavigationContainer>
+	);
 };
 
 export default App;
